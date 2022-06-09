b0014518db15b607e3351d66a4729b84ce605b42
Osama Shbib <osamashbib28@gmail.com>
2022-06-09 14:44:06 +0300

    Setup the website

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+const body = document.querySelector("body");
+const content = document.createElement("div");
+content.id = "content";
+body.appendChild(content);
+
+
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Odin | Restaurant',
+        }),
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    devServer: {
+        static: './dist',
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+};
