7d03da1cfb4648654eb13d72844a1f5a5bc4d816
Osama Shbib <osamashbib28@gmail.com>
2022-06-09 18:09:15 +0300

    Add Header and prepared navbar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import './style.css';
+import createHeader from "./header";
+import createNavbar from './navbar';
 
+// Setting the wrapper div
 const body = document.querySelector("body");
 const content = document.createElement("div");
 content.id = "content";
 body.appendChild(content);
 
-
+createHeader();
+createNavbar();
