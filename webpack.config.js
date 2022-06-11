65c2a621d399fc330c96df78b66b6fcf14985b73
Osama Shbib <osamashbib28@gmail.com>
2022-06-11 19:39:01 +0300

    Final

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
-    devtool: 'inline-source-map',
     plugins: [
         new HtmlWebpackPlugin({
           title: 'Odin | Restaurant',
